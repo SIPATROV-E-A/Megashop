bd28f7cd231cdbb00046a0717c50542667799adb
Evgeny <sipatrov_e@mail.ru>
2023-05-20 10:42:40 +0400

    favorite

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./index.css"
-import heart from '../assets/heart.svg'
+import {ReactComponent as Heart} from '../assets/heart.svg'
 
-const Card =({name, discount, price, pictures})=>{
+const Card =({name, discount, price, pictures,stock, isFavorite })=>{
 
-    const sale = price - (price/100)*discount;
+    const sale = Math.round(price - (price/100)*discount);
     
     // ?? - оператор нулевого слияния. Если price = null/andefjund , то выполняется правая часть
     // !! - привести правую часть к булевому значению
     // {...&& ...} - условный рендеринг - если левая часть true, то правая часть рендерится, иначе не рендерится
-    
+    // &nbsp; - неразрывный прбел, не дает перенести сиволы на другую строчку от зависимого элемента
 return(
     <div className="card_container">
         <div className="sale_container">
@@ -27,13 +27,16 @@ return(
          </div>
          <div> рейтинг </div>
          <div className="prise_container">
-            <span className="price" > {price ?? 'no price'} Р.</span> 
-            <span className="price_sale"> {sale} Р.</span>
+            <span className="price" > {price ?? 'no price'}&nbsp; Р.</span> 
+            <span className="price_sale"> {sale}&nbsp; Р.</span>
 
          </div>
+         <div>
+            {stock}
+         </div>
          <button> в корзину</button>
-         <button>
-            <img src={heart} alt="добавить в избранное" className="heart" />
+         <button className="favorite_ikon">
+            <Heart className={isFavorite ? 'favorite_ikon' : 'heart'}/>
          </button>
     </div>
 )
