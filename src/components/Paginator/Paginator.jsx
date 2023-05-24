a1084b4f7fd5893213a48d8bf5c3e9a0bef30ac6
Evgeny <sipatrov_e@mail.ru>
2023-05-24 23:22:26 +0400

    удалил

--- src/components/Paginator/Paginator.jsx
+++ src/components/Paginator/Paginator.jsx
@@ -2,19 +2,3 @@ import "./index.css";
 import React from "react";
 
 
-export function Paginator(params){
-    return(
-        <header className="header">
-        <div className="container">
-            <a href="#">
-                <img className="image" src="https://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png" alt="" />
-            </a>
-            <div className="header_container">
-                <input className="input" type="text" placeholder="       поиск" />
-<button className="button" > Поиск</button>        
-            </div>
-        </div>
-
-        </header>
-    );
-}
